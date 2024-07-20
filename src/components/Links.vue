7bbc7178fbe2a09c2c0ba79f84e2418f5b2f0ac7
MIFSH <Z18538383923@gmail.com>
2024-07-20 17:57:11 +0800

    profile

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Blog, Brain, Cloud, ImagesRegular, Staylinked, Image, LaptopCode, Link } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -64,11 +64,11 @@ const siteLinksList = computed(() => {
 // 网站链接图标
 const siteIcon = {
   Blog,
+  Brain,
   Cloud,
-  CompactDisc,
-  Compass,
-  Book,
-  Fire,
+  ImagesRegular,
+  Staylinked,
+  Image,
   LaptopCode,
 };
 
